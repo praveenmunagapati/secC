23d9b1affafc75c3834a3e6020b836e4ccc7e236
praveenmun <mpraveenpro@gmail.com>
2026-09-10 11:30:57 +0530

    Refactor code and update inspection settings
    - Updated C code in main.c to simplify logical operations and remove commented-out code.
    - Adjusted VCS directory mapping in vcs.xml for better project structure.
    - Enhanced editor.xml with new inspection severities for improved code quality checks.

--- main.c
+++ main.c
@@ -3,139 +3,28 @@
 
 int main(void) {
     setbuf(stdout,0);
-    // c can process only 3 types
-    /* integers
-     * fractions
-     * character
+    //logical
+    // && || !
+    // logical and
+    /*      a   b   &&   ||
+     *       0   0   0    0
+     *       0   1   0    1
+     *       1   0   0    1
+     *       1   1   1    1
      */
-    //ipo
-    //i input
-    // p processing
-    //o output
+    int num1 = 15;
+    int num2 = 14;
+    printf("%d\n",!(num1>num2));
 
-   //  //arthematic
-   //  // + - / * %
-   //
-   //  // int a = 5,b = 6;
-   //  int a;
-   //  a = 5;
-   //  int b;
-   //  b = 6;
-   //  int c;
-   //  c = 15;
-   //  // +  is binary operator
-   //  int sum = a + b;
-   //  printf("%d\n", sum);
-   //  int sumc = a + b + c + 5;
-   //  printf("%d\n", sumc);
-   //
-   //  int d = 10;
-   //
-   //  int minusab = a - b + c;
-   //  printf("%d\n", minusab);
-   //
-   //  int mul = a * b;
-   //  printf("%d\n", mul);
-   //
-   //  int mixl = a + b * d - c;
-   //  // int mixl = b * d + a;
-   //  printf("%d\n", mixl);
-   //
-   //  // division
-   //  // /
-   //  // data conversion
-   //  //implisit short -> int -> long
-   //  float x = 19/9;
-   //  printf("%f\n", x);
-   //  short s = 15;
-   //  int j = s;
-   // printf("%d\n",s);
-   //  unsigned int integer = 4294967295;
-   //  printf("%u\n",integer);
-   //  unsigned short shortint = integer;
-   //  //bin(1234567890)
-   //  //'1001001100101100000001011010010'
-   //  //bin(722)
-   //  //'1011010010'
-   //  printf("%u\n",shortint);
-   //
-   //  int f = 42949.67295;
-   //  printf("%d\n",f);
-   //
-   //  x = (float)s/9;
-   //  printf("%f\n", x);
-   //  printf("%f\n",(float)22/7);
-   //
-   //  a = 5;
-   //  b = 6;
-   //  c = 7;
-   //  d = 8;
-   //  //            5  +    48 -     1
-   //  float  mixp = a + b * d - c / 5;
-   //  printf("%f\n",mixp);
-   //
-   //  int div = 11/2;
-   //  int mod = 4%3;
-   //  printf("%d\n",div);
-   //  printf("%d\n",mod);
-   //  a = 5;
-   //  b = 6;
-   //  c = 7;
-   //  d = 8;
-   //  float  mixm = a + b * d - c / 5  % 11;
-   //  printf("%f\n",mixm);
+    int num3 = 5;
+    printf("%d\n",(num1 > num2)&&(num1 > num3));
 
-    //inc dec
-    //++ --
-    // pre inc
-    // int x = 10;
-    // int preinc = ++x;
-    // // x = x + 1 - > int preinc = x;
-    // printf("%d\n",x);
-    // printf("%d\n",preinc);
-    // //pre dec
-    // int y = 10;
-    // int predec = --y;
-    // // y = y - 1 - > int predec = y ;
-    // printf("%d\n",x);
-    // printf("%d\n",predec);
-    // int z = 10;
-    // int postinc = z++;
-    // // int postinc = z; -> z = z + 1
-    // printf("%d\n",z);
-    // printf("%d\n",postinc);
-    // //pre dec
-    // int h = 10;
-    // int postdec = h--;
-    // //  int postdec = h ; - > h = h - 1
-    // printf("%d\n",h);
-    // printf("%d\n",postdec);
-    // printf("%d\n",++y + ++x - z-- - x++);
-    // // =
-    // int g = 15+1;
-    // int shortp = 10;
-    // shortp += 5;
-    // // shortp = shortp + 5;
-    // printf("%d\n",shortp);
+    int num4 = 7;
+    printf("%d\n",(num1 > num2)&&(num1 > num3)&&(num1 >num4));
 
-    //relational
-    // > < >= <= == !=
-    int a;
-    a = 5;
-    int b;
-    b = 6;
-    int c;
-    c = 15;
-    printf("%d\n",a>b);
-    printf("%d\n",c>a);
-    printf("%d\n",a<b);
-    printf("%d\n",c<a);
-    printf("%d\n",c>=a);
-    printf("%d\n",c<=a);
-    printf("%d\n",c==a);
-    printf("%d\n",c!=a);
+    printf("%d\n",(0||1));
+    printf("%d\n",!((num1 > num2)||(num1 > num3)&&(num1 >num4)));
 
-    
 
     return 0;
 }
