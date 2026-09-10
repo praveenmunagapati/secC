55851b7397346f91af2bc08ae5601793cf823fd4
praveenmun <mpraveenpro@gmail.com>
2026-09-10 11:52:14 +0530

    Refactor logical operations examples to focus on bitwise operations in main.c

--- main.c
+++ main.c
@@ -3,28 +3,36 @@
 
 int main(void) {
     setbuf(stdout,0);
-    //logical
-    // && || !
-    // logical and
-    /*      a   b   &&   ||
-     *       0   0   0    0
-     *       0   1   0    1
-     *       1   0   0    1
-     *       1   1   1    1
-     */
-    int num1 = 15;
-    int num2 = 14;
-    printf("%d\n",!(num1>num2));
+    //bitwise
+    // & | ^ ~ << >>
 
-    int num3 = 5;
-    printf("%d\n",(num1 > num2)&&(num1 > num3));
-
-    int num4 = 7;
-    printf("%d\n",(num1 > num2)&&(num1 > num3)&&(num1 >num4));
-
-    printf("%d\n",(0||1));
-    printf("%d\n",!((num1 > num2)||(num1 > num3)&&(num1 >num4)));
-
+    printf("%d\n",17&5);
+    // 7   111
+    //      &
+    // 5   101
+    //     101 -> 5
+    // 17   1010 + 111
+    //   17    10001
+    //    5    00101
+    //         00001
+    printf("%d\n",17|5);
+    //   17    10001
+    //    5    00101
+    //          10101
+    printf("%d\n",17^5);
+    //   17    10001
+    //    5    00101
+    //         10100
+    printf("%d\n",~500000);
+    /*
+     *    1 byte integer type
+     *    2p8 256 values
+     *    non negetive 0 255
+     *    both p n -128 to -1 and 0 127
+     *    -1 0
+     *     -2 1
+           -3 2
+*/
 
     return 0;
 }
