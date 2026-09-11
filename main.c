0d2358ddae3183d296af6f502f3dd814ce4b1cf4
praveenmun <mpraveenpro@gmail.com>
2026-09-11 13:42:35 +0530

    Refactor conditional statements in main.c to simplify logic and improve readability

--- main.c
+++ main.c
@@ -5,16 +5,7 @@ int main(void) {
     setbuf(stdout,0);
     //if else switch case default break
 
-    int a = 100 ,b = 15;
-    int result = a>b;
-    if (result) {
-        printf("a is big");
-    }
-    result = b>a;
-    if (result) {
-        printf("b is big");
-    }
-    int c = 16;
+    int a = 100 ,b = 15,c = 16;
     if (a>b) {
         if (a>c) {
             printf("a is big");
