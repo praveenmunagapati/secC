90b08e804934c982e217f1f3b92885a4da304e49
praveenmun <mpraveenpro@gmail.com>
2026-09-11 13:54:27 +0530

    Refactor nested conditional statements in main.c to improve readability

--- main.c
+++ main.c
@@ -4,22 +4,15 @@
 int main(void) {
     setbuf(stdout,0);
     //if else switch case default break
-
     int a = 100 ,b = 15,c = 16;
-    if (a>b) {
-        if (a>c) {
-            printf("a is big");
-        }
+    if ((a>b)&&(a>c) ){
+        printf("a is big");
     }
-    if (b>a) {
-        if (b>c) {
-            printf("c is big");
-        }
+    if ((b>a)&&(b>c)) {
+        printf("c is big");
     }
-    if (c>a) {
-        if (c>b) {
-            printf("c is big");
-        }
+    if (c>a&&c>b) {
+        printf("c is big");
     }
 
     return 0;
