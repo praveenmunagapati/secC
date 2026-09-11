4b1cebc05bc38016356d3097eab4d18e392d27ee
praveenmun <mpraveenpro@gmail.com>
2026-09-11 13:40:20 +0530

    Refactor conditional statements in main.c to improve readability and remove commented-out code

--- main.c
+++ main.c
@@ -3,14 +3,33 @@
 
 int main(void) {
     setbuf(stdout,0);
-    //// a + - / * %
-    // inc dec ++ --
-    // = ,
-    // += -= *= /= a+=1 = a =a +1
-    // relational > < >= <= != ==
-    //logical && || !
-    //bitwise & | ^ ~ <<  >>
+    //if else switch case default break
 
-    
+    int a = 100 ,b = 15;
+    int result = a>b;
+    if (result) {
+        printf("a is big");
+    }
+    result = b>a;
+    if (result) {
+        printf("b is big");
+    }
+    int c = 16;
+    if (a>b) {
+        if (a>c) {
+            printf("a is big");
+        }
+    }
+    if (b>a) {
+        if (b>c) {
+            printf("c is big");
+        }
+    }
+    if (c>a) {
+        if (c>b) {
+            printf("c is big");
+        }
+    }
+
     return 0;
 }
