2ed2705e1bb21045368150131247f8243af26d46
praveenmun <mpraveenpro@gmail.com>
2026-09-11 14:43:00 +0530

    Refactor main.c to improve formatting and readability of conditional statements

--- main.c
+++ main.c
@@ -2,30 +2,37 @@
 #include <stdio.h>
 
 int main(void) {
-    setbuf(stdout,0);
+    setbuf(stdout, 0);
     //if else switch case default break
+    setbuf(stdout,0);
     int i = 10;
-    if (i==1) {
+    if (i == 1) {
         printf("one");
-    }else if (i == 2) {
-                printf("two");
-            }else if (i==3) {
-                        printf("three");
-                    }else if (i==4) {
-                                printf("four");
-                            }else if (i == 5) {
-                                        printf("five");
-                                    }else {
-                                        printf("no suitable value detected");
-                                    }
+    } else if (i == 2) {
+        printf("two");
+    } else if (i == 3) {
+        printf("three");
+    } else if (i == 4) {
+        printf("four");
+    } else if (i == 5) {
+        printf("five");
+    } else {
+        printf("no suitable value detected");
+    }
 
     switch (i) {
-        case 1 : printf("one");break;
-        case 2 : printf("two");break;
-        case 3 : printf("three");break;
-        case 4 : printf("four");break;
-        case 5 : printf("five");break;
-        default:printf("no suitable value detected");break;
+        case 1: printf("one");
+            break;
+        case 2: printf("two");
+            break;
+        case 3: printf("three");
+            break;
+        case 4: printf("four");
+            break;
+        case 5: printf("five");
+            break;
+        default: printf("no suitable value detected");
+            break;
     }
 
     return 0;
