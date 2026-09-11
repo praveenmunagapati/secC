1482a6987550eb1b749c15585a557e6c701f1ff6
praveenmun <mpraveenpro@gmail.com>
2026-09-11 14:47:53 +0530

    Refactor main.c to replace if-else statements with switch-case for improved readability

--- main.c
+++ main.c
@@ -4,34 +4,14 @@
 int main(void) {
     setbuf(stdout, 0);
     //if else switch case default break
-    setbuf(stdout,0);
-    int i = 10;
-    if (i == 1) {
-        printf("one");
-    } else if (i == 2) {
-        printf("two");
-    } else if (i == 3) {
-        printf("three");
-    } else if (i == 4) {
-        printf("four");
-    } else if (i == 5) {
-        printf("five");
-    } else {
-        printf("no suitable value detected");
-    }
-
+    int i = 2;
     switch (i) {
-        case 1: printf("one");
-            break;
-        case 2: printf("two");
-            break;
-        case 3: printf("three");
-            break;
-        case 4: printf("four");
-            break;
-        case 5: printf("five");
-            break;
-        default: printf("no suitable value detected");
+        case 1: printf("one\n");break;
+        case 2: printf("two\n");break;
+        case 3: printf("three\n");break;
+        case 4: printf("four\n");break;
+        case 5: printf("five\n");break;
+        default: printf("no suitable value detected\n");
             break;
     }
 
