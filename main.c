1b34e704e08b24b03a351dcf188f5467d9b2adb0
praveenmun <mpraveenpro@gmail.com>
2026-09-16 14:40:21 +0530

    Refactor main.c to implement prime number checking for numbers 1 to 1000

--- main.c
+++ main.c
@@ -3,24 +3,25 @@
 int main(void) {
     setbuf(stdout, 0);
    // b) Write a program that finds if a given number is a prime number.
-    int num = 1;
-    if (num<=0) {
-        printf("not qualified of prime");
-    }else {
-        int count = 0;
-        for (int i = 1 ; i<=num ; i++) {
-            // printf("%d mod %d = %d\n",num,i,num%i);
-            if (num%i==0) {
-                count++;
+    for (int j = 1;j<=1000;j++) {
+        int num = j;
+        if (num<=0) {
+            printf("not qualified of prime\n");
+        }else {
+            int count = 0;
+            for (int i = 1 ; i<=num ; i++) {
+                // printf("%d mod %d = %d\n",num,i,num%i);
+                if (num%i==0) {
+                    count++;
+                }
+            }
+            // printf("%d",count);
+            if (count<=2) {
+                printf("%d is prime\n",num);
+            }else {
+                printf("%d is not prime\n",num);
             }
         }
-        // printf("%d",count);
-        if (count<=2) {
-            printf("%d is prime",num);
-        }else {
-            printf("%d is not prime",num);
-        }
     }
-
     return 0;
 }
