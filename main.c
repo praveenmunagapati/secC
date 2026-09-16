169b70dc31e44aa9ed65b8352c87622b21475954
praveenmun <mpraveenpro@gmail.com>
2026-09-16 14:08:43 +0530

    Refactor main.c to remove multiplication table code and implement binary equivalent display for a given number

--- main.c
+++ main.c
@@ -2,19 +2,13 @@
 
 int main(void) {
     setbuf(stdout, 0);
-    /*
-    c) Write a program that prints a multiplication table for a given number and the number of rows in
-        the table. For example, for a number 5 and rows = 3, the output should be:
-        5 x 1 = 5
-        5 x 2 = 10
-        5 x 3 = 15
-*/
-    int tableno ,rows;
-    tableno = 5;
-    rows = 3;
-    for (int i = 1; i <= rows; ++i) {
-        printf("%d x %d = %d\n",tableno,i,tableno*i);
+   // d) Write a program
+   // that shows the binary equivalent of a given
+   // positive number between 0 to 255.
+    unsigned int num;
+    num = 2;
+    for (unsigned int i = 128;i>0;i >>= 1) {
+        printf("%u",(num&i)>=1?1:0);
     }
-
     return 0;
 }
