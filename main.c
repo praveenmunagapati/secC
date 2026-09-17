d4fed885dc722ffbd7f7c1459fd5c4a41d648e9a
praveenmun <mpraveenpro@gmail.com>
2026-09-17 11:42:20 +0530

    Refactor main.c to implement simple interest calculation and remove prime number checking logic

--- main.c
+++ main.c
@@ -1,27 +1,56 @@
 #include <stdio.h>
+//functions
+//syntax
+/*  return type functionname(parameters [only declaration ] ); signature
+ *  {
+ *
+ *      statements
+ *      return return type;
+    }
+    */
+//b) Write the program for the simple, compound interest.
 
+//4tytpes
+void wopwor(void) {
+    printf("no para no return\n");
+    return;
+}
+void wpwor(int x) {
+    printf("%d\n",x);
+    return;
+}
+
+int wopwr(void) {
+    return 127;
+}
+int wpwr(int a , int b) {
+    return a+b;
+}
+//simple interrest
+// si = ptr/100
+
+float simpleinterest(int principle,int time,float rate);
 int main(void) {
     setbuf(stdout, 0);
-   // b) Write a program that finds if a given number is a prime number.
-    for (int j = 1;j<=1000;j++) {
-        int num = j;
-        if (num<=0) {
-            printf("not qualified of prime\n");
-        }else {
-            int count = 0;
-            for (int i = 1 ; i<=num ; i++) {
-                // printf("%d mod %d = %d\n",num,i,num%i);
-                if (num%i==0) {
-                    count++;
-                }
-            }
-            // printf("%d",count);
-            if (count<=2) {
-                printf("%d is prime\n",num);
-            }else {
-                printf("%d is not prime\n",num);
-            }
-        }
-    }
+    wopwor();
+    wpwor(1500);
+    int i = wopwr();
+    printf("%d\n",i);
+    int sum  = wpwr(5,6);
+    printf("%d\n",sum);
+    int principle = 100000;
+    int time = 12;
+    float rate = 2.5;
+    float si = simpleinterest(principle,time,rate);
+    printf("%f\n",si);
     return 0;
+}
+float simpleinterest(int principle,int time,float rate) {
+    // int principle = 100000;
+    // int time = 12;
+    // float rate = 2.5;
+    //
+    float si = (principle * time * rate )/100;
+    return si;
+    // printf("%f\n",si);
 }
