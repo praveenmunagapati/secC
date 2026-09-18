78edb33e2bca6de4a8fb72085f7b8c7fea12b266
praveenmun <mpraveenpro@gmail.com>
2026-09-18 13:51:38 +0530

    Refactor main.c to simplify code by removing unused variables and demonstrating call by value

--- main.c
+++ main.c
@@ -1,40 +1,17 @@
 #include <stdio.h>
+//call by value
+void print(int h) {
+  printf("in print h %p\n",&h);
+  printf("%d\n",h);
+  return;
+}
 int main(void) {
   setbuf(stdout, 0);
-  int k = 10;
-  printf("%d\n",k);
-  printf("%p\n",&k);
-  printf("%d\n",*(&k));
-
-  //what is pointer variable holds only address
-  int *pk;
-  pk = &k;
-
-  printf("%p\n",&pk);
-  printf("%p\n",pk);
-  printf("%p\n",&k);
-  printf("%d\n",*pk);
-  k = 15;
-  printf("%d\n",k);
-  *pk = 25;
-  printf("%d\n",k);
-  //errors
-   float f = 1.6;
-//   pk = f;
-   long l = 17875;
-//   pk = &l;
-short s = 5;
-//   pk = &s;
-  int g = 105;
-  pk = &g;
-  printf("%d\n",*pk);
-  void *gp ;
-    gp = &f;
-    printf("%f\n",*(float *)gp);
-    gp = &l;
-    printf("%d\n",*(long *)gp);
-    gp = &s;
-    printf("%d\n",*(short *)gp);
+  int h = 15;
+  printf("in main h %p\n",&h);
+  print(h);
+  h = 20;
+  printf("in main h %d\n",h);
 
 
 }
