e7f4784d5a1d5dbd9f817568172ef6fea577d11e
praveenmun <mpraveenpro@gmail.com>
2026-09-18 13:58:32 +0530

    Refactor main.c to enhance call by reference demonstration and improve variable address output

--- main.c
+++ main.c
@@ -5,13 +5,23 @@ void print(int h) {
   printf("%d\n",h);
   return;
 }
+//call by reference
+void printr(int *ph) {
+  printf("in print ph address %p\n",ph);
+  printf("%d\n",*ph);
+  *ph = 89;
+  return;
+}
 int main(void) {
   setbuf(stdout, 0);
   int h = 15;
   printf("in main h %p\n",&h);
   print(h);
   h = 20;
-  printf("in main h %d\n",h);
+  printf("in main h  addresss %p\n",&h);
 
+  printf("in main h %d\n",h);
+printr(&h);
+  printf("in main h value after printr %d\n",h);
 
 }
