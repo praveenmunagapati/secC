0629b2e38a9a8bbea31542a3965d2e9746a5556f
praveenmun <mpraveenpro@gmail.com>
2026-09-23 14:44:24 +0530

    Refactor main.c to remove enum definitions and related logic; streamline multidimensional array handling and output

--- main.c
+++ main.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
-enum WEEK {
-    MONDAY=-7,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
-};
+
 int main(void) {
     setbuf(stdout, 0);
-    printf("%d\n",sizeof(enum WEEK));
-    printf("%d\n",sizeof(MONDAY));
-    printf("%d\n",MONDAY);
-    printf("%d\n",TUESDAY);
-    printf("%d\n",WEDNESDAY);
-    printf("%d\n",THURSDAY);
-    printf("%d\n",FRIDAY);
-    printf("%d\n",SATURDAY);
-    printf("%d\n",SUNDAY);
-    //MONDAY = 15;//CANT THEY ARE NOT VARIABLES THEY ARE KEY VALUE PAIRS
+    //multidimentional array
 
-    //machine tea coffee boost
-    // 0 for tea 1 coffee 2 for boost
-    int choice = 4;
-    switch (choice) {
-        case 1 : printf("you get tea");break;
-        case 2 : printf("you get coffee");break;
-        case 3 : printf("you get boost");break;
-        default:printf("no choice\n");break;
+    int array2d[2][2] = {{3,4},{5,6}};
+    printf("%d\n",sizeof(array2d));
+    array2d[0][0] = 1;
+    array2d[0][1] = 2;
+    array2d[1][0] = 3;
+    array2d[1][1] = 4;
+    printf("array2d matrix \n");
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            printf("%d\t",array2d[i][j]);
+        }
+        printf("\n");
     }
-    enum  MACHINE {
-        TEA = 1,
-        COFFEE,
-        BOOST
-    };
-    choice = COFFEE;
-    switch (choice) {
-        case TEA : printf("you get tea");break;
-        case COFFEE : printf("you get coffee");break;
-        case BOOST : printf("you get boost");break;
-        default:printf("no choice");break;
+    int another2d[2][2] = {{4,9},{11,6}};
+    printf("another2d matrix \n");
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            printf("%d\t",another2d[i][j]);
+        }
+        printf("\n");
     }
-
+    int result2d[2][2];
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            result2d[i][j] = array2d[i][j] + another2d[i][j];
+        }
+    }
+    printf("result matrix \n");
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            printf("%d\t",result2d[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
